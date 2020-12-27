78a40e0f866b5498ca5afdf809f57b68fb1a836f
Jerin Philip <jerin.philip@research.iiit.ac.in>
2020-12-27 16:46:55 +0000

    changes

--- padding/main.hs
+++ padding/main.hs
@@ -10,12 +10,18 @@ import Data.List (unfoldr, sort)
 zeroElement = square 2 # lc black # fc red # lw 0.1
 nonZeroElement = square 2 # lc black # fc green # lw 0.1
 
-paddedSequence maxLength length = hsep 1 $ replicate length nonZeroElement ++ replicate (maxLength - length) zeroElement
-paddedSequences batchSize maxLength sequenceLengths = vsep 1 $  map (paddedSequence maxLength) sequenceLengths
+paddedSequence maxLength length = 
+        hsep 1 $ (++)
+                    (replicate length nonZeroElement )
+                    (replicate (maxLength - length) zeroElement)
 
-demo :: Int -> Int -> [Int] -> Diagram B
-demo batchSize maxLength sequenceLengths = pad 2 $ paddedSequences batchSize maxLength sequenceLengths
+paddedSequences maxLength sequenceLengths = 
+        vsep 1 $ map (paddedSequence maxLength) sequenceLengths
 
+demo :: Int -> [Int] -> Diagram B
+demo maxLength sequenceLengths = pad 2 $ paddedSequences maxLength sequenceLengths
+
+-- https://stackoverflow.com/q/30740366/4565794
 randomList :: Int -> Int -> Int -> IO [Int]
 randomList n lb ub = sequence $ replicate n $ randomRIO (lb, ub)
 
@@ -23,4 +29,4 @@ main = do
     let maxLength = 16
     let batchSize = 20
     sequenceLengths <- randomList batchSize 1 maxLength
-    mainWith $ demo batchSize maxLength $ sort sequenceLengths
+    mainWith $ demo maxLength $ sort sequenceLengths
